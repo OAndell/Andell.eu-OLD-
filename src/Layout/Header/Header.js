e66637c717c3842fe516d7b0d265c194f005d46b
Oscar <oscar@andell.eu>
2017-06-05 11:02:52 +0200

    header

--- src/Layout/Header/Header.js
+++ src/Layout/Header/Header.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//TODO: Backgrundsbild??
 export default class Header extends React.Component {
   render() {
-	var headStyle = {
-		color:'white',
-		textAlign: 'center',
-		lineHeight: '90%', margin: 'auto',
-		width: '90%'};
+	var headerBackgroundStyle = {
+	
+		background: 'linear-gradient(to bottom right, black, rgb(55, 55, 55))',
+		lineHeight: '70%',
+		margin: 'auto',
+		height: '125px',
+		borderRadius: '5px',
+		border: '2px solid grey'};
+	
+	var textStyle = {
+		color:'White',
+		float: 'right',
+		margin: '20px',
+		fontFamily: 'Ubuntu-sansSerif'
+	}
 		
     return(
-		<div style={headStyle}>
+		<div className="header" style={headerBackgroundStyle}>
 			<br/>
-			<h1>Header Text</h1>
-			<h2>texti texti</h2>
-			<br/>
+			<div style={textStyle}>
+				<h1>Header Text</h1>
+				<p>some text</p>
+			</div>
+			
 		</div>
 	);
   }
