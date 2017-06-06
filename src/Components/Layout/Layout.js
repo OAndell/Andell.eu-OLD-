0028f033569c7c442e1b84823636c4f67eddb333
Oscar <oscar@andell.eu>
2017-06-06 22:40:19 +0200

    Fixed that mess...

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from '../../Components/Header/Header';
-<<<<<<< HEAD
 import BackgroundImage from '../../Resources/Backgrounds/background4.png';
-//import Drawer from "./Drawer/Drawer"
-=======
-import BackgroundImage from '../../Resources/Backgrounds/background2.png';
-import Imagee from "../../Networking/NetworkImg"
->>>>>>> origin/master
 import MainBody from '../../Components/MainBody/MainBody';
 import Drawer from '../../Components/Drawer/Drawer';
 
@@ -22,13 +16,13 @@ export default class Layout extends React.Component {
       <div id ="outer-container">
       <Drawer/>
       <main id ="page-wrap">
-    		<div style={layoutStyle}>
-          < Header largeText = "Header Text" smallText = "asdasd"/>
-        <Imagee/> 
-            <MainBody/>
-    	  </div>
+    	<div style={layoutStyle}>
+			  < Header largeText = "Header Text" smallText = "asdasd"/>
+			  <MainBody/>
+		  </div>
       </main>
       </div>
 	);
   }
 }
+ 
