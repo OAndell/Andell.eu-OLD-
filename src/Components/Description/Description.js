c80e61df588744d6eba00459570bf7c896a2822a
Oscar <oscar@andell.eu>
2017-06-08 15:19:57 +0200

    More work on personal pages and design

--- src/Components/Description/Description.js
+++ src/Components/Description/Description.js
@@ -5,12 +5,37 @@ import './Description.css';
 export default class Description extends React.Component {
 	constructor(props){
 		super(props)
+		this.lang  = props.lang
+		this.CV = props.CV
+		console.log(this.CV.toString());
+		this.parseJson = this.parseJson.bind(this);
 	}
+	
+	
+	parseJson(){
+		var sections = [];
+		for (var key in this.CV) {
+			var value = this.CV[key];
+			sections.push(
+				<div>
+					<h2><i className={value.icon}/> {value.title}</h2>
+					<h3>{value.underTitle}</h3>
+					<p className="date">{value.date}</p>
+					<p>{value.summary}</p>
+					<br/>
+				</div>
+			);
+		}	
+		return(
+			<div>{sections}</div>
+		);
+	}
+	
 
   render() { 
     return(
 			<div className="resumeHolder">
-				
+					{this.parseJson()}
 			</div>
 	);
   }
